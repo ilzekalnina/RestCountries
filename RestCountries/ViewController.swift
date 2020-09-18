341ed6b65424a6a5605d17c149ec5eea498dc7fd
Ilze Kalnina <ilze@skate.lv>
2020-09-18 14:15:46 +0300

    Initial Commit

---
+++ RestCountries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestCountries
+//
+//  Created by Kisacka on 18/09/2020.
+//  Copyright © 2020 Kisacka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
